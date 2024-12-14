723153a5723694b4e71830d988468926d4fefcf3
Ning Wang <30329277@qq.com>
2024-12-14 10:33:47 +0800

    add gitignore

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CsharpInterpoWithVBA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建Excel应用程序对象
+            Excel.Application excelApp = new Excel.Application();
+            // 打开Excel文件
+            Excel.Workbook workbook = excelApp.Workbooks.Open(@"D:\DotNetProject\forCsharpTest.xlsm");
+
+            // 调用宏
+            excelApp.Run("Module1.Test1");
+
+            // 获取第一个工作表
+            Excel.Worksheet worksheet = workbook.Sheets["Sheet1"];
+            // 读取单元格A1的值
+            string cellValue = worksheet.Range["A1"].Value.ToString();
+
+            // 打印单元格A1的值到控制台
+            Console.WriteLine("Value in Sheet1 A1: " + cellValue);
+
+            // 关闭工作簿
+            workbook.Close(false);
+            // 退出Excel应用程序
+            excelApp.Quit();
+
+            // 释放COM对象
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            Console.WriteLine("Macro executed successfully.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CsharpInterpoWithVBA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsharpInterpoWithVBA")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dbfbed4c-52da-4970-b0f0-1299a24471ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
